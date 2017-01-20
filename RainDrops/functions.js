bb9a8f3448659e2d59a1ae35d44649d791f3b98f
PingNPong <pong@Nvyve.com>
2017-01-20 16:50:40 -0500

    Updating changes
    Changes Updated

--- RainDrops/functions.js
+++ RainDrops/functions.js
@@ -81,7 +81,7 @@ function FadeTransition1(ImageName,xOffset,Rotation,time,delayTime)
 
 function SetPosition(ImageName,xOffset,yTarget,Rotation,delayTime)
 {
-	$(ImageName).transition({x: xOffset,y:yTarget, rotate:''+Rotation+'deg',delay: delayTime},5,"ease");
+	$(ImageName).transition({x: xOffset,y:yTarget, rotate:''+Rotation+'deg',delay: delayTime},80,"ease");
 }
 
 function returnPreviousInt(index,currentImg,maxImages)

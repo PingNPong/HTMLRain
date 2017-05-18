23cd5841acc7d81abf7bb5a8209e451a6fdf6bc3
PingNPong <pong@Nvyve.com>
2017-05-18 14:50:50 -0400

    Adding in resized Space
    Resized to 1920x 800 (21:9)And trying new images

---
+++ StaticRain800/ReadTextFile.js
@@ -0,0 +1,90 @@
+var file = new File(["boringContent"], "http://192.168.2.209/PhotoBooth/Mod.txt");
+var reader = new FileReader();
+var fileName="http://192.168.2.209/PhotoBooth/Mod.txt";
+var currentNumber=0;
+var LastNumber=0;
+
+function ReadFiles()
+{
+	$.ajax({
+        url: "http://192.168.2.209/PhotoBooth/LastModified/Mod.txt",//"http://192.168.2.209/PhotoBooth/Mod.txt",
+        dataType: "text",  // jQuery will infer this, but you can set explicitly
+        success: function (data)
+		{
+			currentNumber=data;
+			//console.log("reading File"+ currentNumber+ " vs "+LastNumber);
+			if (currentNumber!=LastNumber)
+			{// if the file number is not the same 
+				//console.log("Not the same number  "+currentNumber+"!="+LastNumber);
+				PreJumpImg = currentImg;
+				currentImg = 16;	// reset the numbers;
+				console.log("prejump="+PreJumpImg);
+				LastNumber=currentNumber;
+				ImgState=2;
+				// changing the timings
+				totalDuration=2000;
+				waitDuration=1000;
+				ExtraLoading=true;
+			}
+		}
+    });
+
+	// if (currentNumber!=LastNumber)
+	// {// if the file number is not the same 
+		// console.log("Not the same number  "+currentNumber+"!="+LastNumber);
+		// PreJumpImg = currentImg;
+		// currentImg = 0;	// reset the numbers;
+		// console.log("prejump="+PreJumpImg);
+		// LastNumber=currentNumber;
+		// ImgState=3;
+	// }
+	
+	// $.get('http://192.168.2.209/PhotoBooth/Mod.txt').then(function(data) {
+	  // var lines = data.split('\n');
+	  // var index = Math.floor(Math.random() * lines.length);
+	  // console.log(lines[index]);
+	// });
+	// reader.readAsText(file);
+	// reader.onload = function (e) {
+		// console.log("test "+reader.result);
+		// //output = e.target.result;
+		// //displayContents(output);
+	// };//end onload()
+}
+
+function OnSuccessLoad(data)
+{
+	currentNumber=data;
+	console.log(currentNumber);
+	
+}
+
+function readText(filePath) {
+	var output = ""; //placeholder for text output
+	if(filePath.files && filePath.files[0]) {           
+		reader.onload = function (e) {
+			output = e.target.result;
+			displayContents(output);
+		};//end onload()
+		reader.readAsText(filePath.files[0]);
+	}//end if html5 filelist support
+	else if(ActiveXObject && filePath) { //fallback to IE 6-8 support via ActiveX
+		try {
+			reader = new ActiveXObject("Scripting.FileSystemObject");
+			var file = reader.OpenTextFile(filePath, 1); //ActiveX File Object
+			output = file.ReadAll(); //text contents of file
+			file.Close(); //close file "input stream"
+			displayContents(output);
+		} catch (e) {
+			if (e.number == -2146827859) {
+				alert('Unable to access local files due to browser security settings. ' + 
+				 'To overcome this, go to Tools->Internet Options->Security->Custom Level. ' + 
+				 'Find the setting for "Initialize and script ActiveX controls not marked as safe" and change it to "Enable" or "Prompt"'); 
+			}
+		}       
+	}
+	else { //this is where you could fallback to Java Applet, Flash or similar
+		return false;
+	}       
+	return true;
+}   
---
+++ StaticRain800/functions.js
@@ -0,0 +1,200 @@
+// all external functions will be stored here 
+// functions can relate to element changing and returning of values 
+// NVYVE
+// Philip Ong   March 17,2017
+var displayConsole = false; 
+
+//===== Changing Images =======
+
+// change the continer background 
+function ChangeContainer(containerName,pictureName) {
+		$(containerName).css('background', 'url('+pictureName+')');
+}
+
+//change a targeted image 
+function ChangeImg(imageName,pictureName) {
+	if (displayConsole)
+	{
+		console.log("Picture name "+pictureName);
+	}
+	
+	try {
+		$(imageName).attr('src', pictureName); // trying to set 
+	}
+	catch(err) {
+		console.log("ChangeImg-Error: "+err.name);
+	}
+	
+}
+
+//===== Video functions =======
+function ChangeVideo(vidName,VideoName)
+{
+	$(vidName).attr('src', VideoName); // trying to set 
+}
+
+function playPause() {
+	var myVideo = document.getElementById("video_container");
+    if (myVideo.paused)
+        myVideo.play();
+    else
+        myVideo.pause();
+}
+
+function playPauseBool(toPlay) {
+	var myVideo = document.getElementById("video_container");
+    if (toPlay)
+        myVideo.play();
+    else
+        myVideo.pause();
+}
+
+function setImageVisible(id, visible) {
+    var img = document.getElementById(id);
+    img.style.visibility = (visible ? 'visible' : 'hidden');
+}
+
+//===== Unused transitions atm =======
+
+// sliding function to animate a container left to right 
+function slideTransitionFromLeft(LeftName, CentreName,time,delayTime)
+{
+	$(LeftName).attr('disabled', false);
+	$(CentreName).attr('disabled', false);
+	// console.log("slideTransition");
+	$(CentreName).transition({x: 1920},10);
+	$(LeftName).transition({x: -5},10);
+	//use this transition rather than animate, better for brightsign 
+	$(LeftName).transition({x: -1930,delay: delayTime},time,"linear");
+	$(CentreName).transition({x: -10,delay: delayTime},time,"linear");
+}	
+
+//setting the out transition of the items NOT used 
+function outTransition(ImageName,xOffset,yTarget,Rotation,time,delayTime)
+{
+	$(ImageName).transition({x: xOffset,y:yTarget,rotate:''+Rotation+'deg',scale: 1},1);
+	$(ImageName).transition({y:500, delay: delayTime,scale: 0.1},time,"ease");
+}
+
+//setting the in transition of the items NOT used 
+function FadeTransition1(ImageName,xOffset,Rotation,time,delayTime)
+{
+	$(ImageName).transition({x: xOffset,y:0, rotate:''+Rotation+'deg',opacity: 100},10,"ease");
+	$(ImageName).transition({ opacity: 0,delay: delayTime },time,"ease");
+}
+
+//===== Used transitions  =======
+
+function SetOpacity(ImageName,SetOpacity,time,delayTime)
+{
+	$(ImageName).transition({x: -10, opacity: SetOpacity, delay: delayTime},time,"ease");
+}
+
+function rainDropTransition(ImageName,yTarget,Rotation,time)
+{
+	$(ImageName).transition({y:yTarget, rotate:''+Rotation+'deg'},time,"ease");
+}
+
+function rainDropTransitionX(ImageName,XTarget,yTarget,Rotation,time)
+{
+	$(ImageName).transition({X:XTarget,y:yTarget, rotate:''+Rotation+'deg'},time,"ease");
+}
+
+//setting the position, rotations and scales quickly
+function SetValues(ImageName,xOffset,yTarget,Rotation,SettingScale,delayTime)
+{
+	$(ImageName).transition({x: xOffset,y:yTarget, rotate:''+Rotation+'deg',scale: SettingScale,delay: delayTime},5);
+}
+
+//setting the position, rotations and scales quickly
+function SetValuesX(ImageName,xOffset,yTarget,Rotation)
+{
+	$(ImageName).transition({x: xOffset,y:yTarget, rotate:''+Rotation+'deg'},3);
+}
+
+function SetValuesMin(ImageName,yTarget,Rotation)
+{
+	$(ImageName).transition({y:yTarget, rotate:''+Rotation+'deg',duration:10});// ({y:yTarget, rotate:''+Rotation+'deg'},1)
+}
+
+function SetValuesNT(ImageName,yTarget,Rotation)
+{
+	//var Location = yTarget+'px';
+	//document.getElementById(ImageName).style.top=yTarget + "px"; 
+	var rotate = 'rotate(' + Rotation + 'deg)';
+    $("#"+ImageName).css({ 
+        '-webkit-transform': rotate
+	});
+}
+
+function SetValuesNT2(ImageName,yTarget,Rotation,time)
+{
+	//var Location = yTarget+'px';
+	//document.getElementById(ImageName).style.top=yTarget + "px"; 
+	var rotate = 'rotate(' + Rotation + 'deg)';
+    $("#"+ImageName).css({ 
+		'-webkit-transition-duration': time,
+        '-webkit-transform': rotate
+	});
+}
+
+function SetCSSValues(ImageName,setWidth,setHeight,setTop,setLeft)
+{
+    $(ImageName).width(setWidth);
+	$(ImageName).height(setHeight);
+	$(ImageName).css 
+	({
+		top: setTop, left:setLeft
+	});
+}
+
+
+
+// === return functions ====
+
+function returnPreviousInt(index,currentImg,maxImages)
+{
+	if ((currentImg-index)<0)
+	{//when the new number will be less than 0 -->
+		//console.log(currentImg-index+" is now "+(maxImages + (currentImg-index)));
+		return maxImages + (currentImg-index);
+	}
+	else 
+	{
+		//console.log(currentImg-index+" is now ");
+		return currentImg-index;
+	}
+}
+
+function RandomNumber(minInt,maxInt,minusInt)
+{
+	var returnNumber = 0;
+	returnNumber=Math.random()* maxInt + minInt;
+	return (returnNumber-minusInt);
+}
+
+function ReturnArrayPosition(EnterNumber)
+{
+	var returningImage; 
+	if (CurrentAngleSet==0)
+	{
+		returningImage=angleArray[CurrentAngleSet*3+EnterNumber];
+	}	
+	else 
+	{
+		var ReturningIndex=CurrentAngleSet*3+EnterNumber;
+		if (3+EnterNumber<6)
+		{
+			//console.log("EN="+EnterNumber+" Less than 7 ="+ReturningIndex+" = "+angleArray[ReturningIndex]);
+			returningImage=angleArray[ReturningIndex];
+		}
+		else 
+		{
+			ReturningIndex=ReturningIndex-6;
+			//console.log("EN="+EnterNumber+" More than 7 ="+ReturningIndex+" = "+angleArray[ReturningIndex]);
+			returningImage=angleArray[ReturningIndex];
+		}
+	}	
+	
+	return returningImage;
+}
